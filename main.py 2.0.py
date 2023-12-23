09782e11cb8a8b30096278445b57859e8bd215bd
gogolyatco <144526524+gogolyatco@users.noreply.github.com>
2023-12-23 13:28:07 +0200

    Add files via upload

---
+++ main.py 2.0.py
@@ -0,0 +1,31 @@
+from PIL import Image
+class ImageEditor():
+    def __init__(self, filename):
+        self.filename = filename
+        self.original = None
+        self.changed= list()
+    def open(self):
+        try:
+            self.original = Image.open(self.filename)
+        except:
+            print("Файл не знайдений")
+        self.original.show()
+    def do_left(self):
+        rotated = self.original.transpose(Image.FLIP_LEFT_RIGHT)
+        self.changed.append(rotated)
+        temp_filename = self.filename.split(".")
+        new_filename = temp_filename[0]+ str(len(self.changed))+".jpg"
+        rotated.save(new_filename)
+    def do_cropped(self):
+        box = (250, 100, 600, 400)
+        cropped = self.original.crop(box)
+        self.changed.append(cropped)
+        temp_filename = self.filename.split(".")
+        new_filename = temp_filename[0] + str(len(self.changed)) + ".jpg"
+        cropped.save(new_filename)
+MyImage = ImageEditor("Screenshot_1.png")
+MyImage.open()
+MyImage.do_left()
+MyImage.do_crooped()
+for im in MyImage.changed:
+    im.show()
